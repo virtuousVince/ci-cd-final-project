3bacd8da148c07becd96b771341aba1d7f56d192
Vince <vincentleung58@gmail.com>
2025-10-17 12:34:56 -0400

    Amend flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,8 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1])
+    counters = [
+        dict(name=count[0], counter=count[1])
         for count in COUNTER.items()]
 
     return jsonify(counters)
@@ -55,7 +56,8 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT,
+        return abort(
+            status.HTTP_409_CONFLICT,
             f"Counter {name} already exists")
 
     COUNTER[name] = 0
@@ -77,8 +79,9 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND,
-        f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -93,8 +96,9 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND,
-        f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 

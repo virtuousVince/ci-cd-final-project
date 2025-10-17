457487c6e9539e4e005ba6d7f1989ddc0bdd2fa2
Vince <vincentleung58@gmail.com>
2025-10-17 12:28:51 -0400

    Amend flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,8 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [dict(name=count[0], counter=count[1])
+        for count in COUNTER.items()]
 
     return jsonify(counters)
 
@@ -54,7 +55,8 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return abort(status.HTTP_409_CONFLICT,
+            f"Counter {name} already exists")
 
     COUNTER[name] = 0
 
@@ -76,7 +78,7 @@ def read_counters(name):
 
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND,
-            f"Counter {name} does not exist")
+        f"Counter {name} does not exist")
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -92,7 +94,7 @@ def update_counters(name):
 
     if name not in COUNTER:
         return abort(status.HTTP_404_NOT_FOUND,
-            f"Counter {name} does not exist")
+        f"Counter {name} does not exist")
 
     COUNTER[name] += 1
 
